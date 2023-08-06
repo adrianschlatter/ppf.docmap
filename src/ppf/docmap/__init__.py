ba949849929c96aea05631fcd97d26d2d885985f
Adrian Schlatter <10478149+adrianschlatter@users.noreply.github.com>
2023-08-06 23:43:33 +0200

    version in __init__.py
    ... and setup.cfg reads it from there (as it's done in ppf.sample)

--- src/ppf/docmap/__init__.py
+++ src/ppf/docmap/__init__.py
@@ -8,6 +8,8 @@ ppf.docmap is a python package to find links in documents and to crawl
 documents to map dependencies between them.
 """
 
+__version__ = '0.1'
+
 # import every function, class, etc. that should be visible in the package
 from .docmap import *
 from .filescanners import *
