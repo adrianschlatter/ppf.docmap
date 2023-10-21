a7554cbfb1eb778814b04488890fec9ac9c845b6
Adrian Schlatter <10478149+adrianschlatter@users.noreply.github.com>
2023-10-21 20:21:42 +0200

    cli: docmap ls
    search refs in a single file

---
+++ src/ppf/docmap/cli.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+
+from plumbum import cli
+from pkg_resources import get_distribution
+from ppf.docmap import mimetype, FileScanner
+from sys import stdout
+from os import linesep
+
+
+class DocMapp(cli.Application):
+    PROGNAME = 'docmap'
+    VERSION = get_distribution('ppf-docmap').version
+    verbose = cli.Flag(['v', 'verbose'],
+                       help='If given, I will be very talkative')
+
+
+@DocMapp.subcommand('ls')
+class DocMappLs(cli.Application):
+    """lists all references found in document"""
+
+    def main(self, doc):
+        # instantiate correct FileScanner class:
+        scanner = FileScanner.registry[mimetype(doc)]()
+
+        # scan document:
+        refs = scanner(doc)
+        for ref in refs:
+            stdout.write(ref + linesep)
+
+
+def main():
+    DocMapp.run()
+
+
+if __name__ == '__main__':
+    main()
